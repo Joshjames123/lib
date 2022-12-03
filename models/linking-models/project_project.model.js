25c820e99d37cd2db58722e8dcb4565497e0f398
joshjames123 <bueranoj@gmail.com>
2022-12-03 20:17:40 +0800

    WIP

---
+++ models/linking-models/project_project.model.js
@@ -0,0 +1,16 @@
+let mongoose = require("mongoose");
+
+module.exports = function () {
+  let Types = mongoose.Schema.Types;
+
+  let Model = {
+    Schema: {
+      friendsSince: {
+        type: Types.Date,
+      },
+    },
+    modelName: "project_project",
+  };
+
+  return Model;
+};
--- models/project.model.js
+++ models/project.model.js
@@ -1,25 +1,48 @@
 module.exports = function (mongoose) {
-  let modelName = "Project";
+  let modelName = "project";
   let Types = mongoose.Schema.Types;
-  let Schema = new mongoose.Schema({
-    projectName: {
-      type: Types.String,
-      required: true,
-      unique: true,
+  let Schema = new mongoose.Schema(
+    {
+      projectName: {
+        type: Types.String,
+        required: true,
+        unique: true,
+      },
+      description: {
+        type: Types.String,
+        required: true,
+      },
+      location: {
+        type: Types.String,
+        required: true,
+      },
+      requirements: {
+        location: { type: Types.String },
+        experience: { type: Types.String },
+        collegeGraduate: { type: Types.Boolean },
+        etc: { type: Types.String },
+      },
+      budget: {
+        min: { type: Types.Number, exclude: true },
+        max: { type: Types.Number, exclude: true },
+      },
     },
-    description: {
-      type: Types.String,
-      required: true,
-    },
-    location: {
-      type: Types.String,
-      required: true,
-    },
-  });
+    { collection: modelName }
+  );
 
   Schema.statics = {
     collectionName: modelName,
-    routeOptions: {},
+    routeOptions: {
+      associations: {
+        projects: {
+          type: "MANY_MANY",
+          alias: "project",
+          model: "project",
+          linkingModel: "project_project",
+          embedAssociation: false,
+        },
+      },
+    },
   };
 
   return Schema;
