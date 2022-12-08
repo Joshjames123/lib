d9638f987ed0a0e8c210d21c7647907ee6c331d1
joshjames123 <bueranoj@gmail.com>
2022-12-09 01:30:37 +0800

    WIP

--- models/applicant.model.js
+++ models/applicant.model.js
@@ -2,16 +2,9 @@ module.exports = function (mongoose) {
   let modelName = "user_applicant";
   let Types = mongoose.Schema.Types;
   let Schema = new mongoose.Schema({
-    email: {
+    resume_file: {
       type: Types.String,
       required: true,
-      unique: true,
-    },
-    password: {
-      type: Types.String,
-      required: true,
-      exclude: true,
-      allowOnUpdate: false,
     },
   });
 
--- models/client.model.js
+++ models/client.model.js
@@ -2,16 +2,9 @@ module.exports = function (mongoose) {
   let modelName = "user_client";
   let Types = mongoose.Schema.Types;
   let Schema = new mongoose.Schema({
-    email: {
+    companyModel: {
       type: Types.String,
       required: true,
-      unique: true,
-    },
-    password: {
-      type: Types.String,
-      required: true,
-      exclude: true,
-      allowOnUpdate: false,
     },
   });
 
